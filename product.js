f66334f8279ffd2e451043dcbe200f0b205fc92f
M. Sasikumar <msasikumarpeace@gmail.com>
2021-10-17 13:51:54 +0530

    Update product.js

--- product.js
+++ product.js
@@ -17,27 +17,34 @@ const editBtn = Array.from(document.getElementsByClassName('btn-success'));
 const saveBtn = Array.from(document.getElementsByClassName('save'));
 const prodName = document.getElementsByClassName('prod-name');
 const productName = document.getElementsByClassName('product-name');
-
+const prodDescription = document.getElementsByClassName('prod-description');
+const productDescription = document.getElementsByClassName('product-description');
 
 
 
 editBtn.forEach((item, i) => {
     item.addEventListener('click', () => {
         prodName[i].style.display = 'none';
+        prodDescription[i].style.display = 'none';
         productName[i].value = prodName[i].textContent;
+        productDescription[i].value = prodDescription[i].textContent;
         editBtn[i].style.display = 'none';
         saveBtn[i].style.display = 'inline-block';
         console.log(productName[i].style.display = 'block');
         productName[i].style.display = 'block';
+        productDescription[i].style.display = 'block';
     })
 })
 
 saveBtn.forEach((item, i) => {
     item.addEventListener('click', () => {
         prodName[i].textContent = productName[i].value;
+        prodDescription[i].textContent = productDescription[i].value;
         prodName[i].style.display = 'inline-block';
+        prodDescription[i].style.display = 'inline-block';
         editBtn[i].style.display = 'inline-block';
         saveBtn[i].style.display = 'none';
         productName[i].style.display = 'none';
+        productDescription[i].style.display = 'none';
     })
 })
